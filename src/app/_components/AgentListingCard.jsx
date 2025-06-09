2f21658522997f33f782e01995a942af0921574d
Avi9631 <avikumarshooters@gmail.com>
2025-06-09 11:28:35 +0530

    dxwd

--- src/app/_components/AgentListingCard.jsx
+++ src/app/_components/AgentListingCard.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {Badge} from "@/components/ui/badge";
 import {Button} from "@/components/ui/button";
-import {ExternalLink, Info, X} from "lucide-react";
+import {Award, CheckLine, ExternalLink, Info, X} from "lucide-react";
 import "./pg-colive.css";
 import Link from "next/link";
 import {useUser} from "@/lib/useUser";
@@ -27,19 +27,60 @@ function AgentListingCard({listing, active}) {
                         <div className="wrapper">
                             <div className="marquee">
                                 <div className="flex overflow-x-auto gap-2">
-                                    <Badge variant="outline" className="rounded-sm">
-                                        2400 sqft.
+                                    {listing?.agentPartnerType ? (<Badge
+                                            className="tracking-wider rounded-sm bg-yellow-600 text-white flex gap-1  shadow">
+                                            <Award size={14}
+                                                   className="text-yellow-300"/><span>{listing?.agentPartnerType}</span></Badge>)
+                                        : <></>}
+                                    {listing?.agentIsVerified ?
+                                        <Badge
+                                            className="tracking-wider rounded-sm bg-green-600 text-white flex gap-1   shadow"><CheckLine
+                                            size={14}/><span>Verified</span></Badge>
+                                        : <></>}
+                                    <Badge variant="outline" className="rounded-sm shadow">
+                                        Total Listing : {listing?.agentTotalListings}
                                     </Badge>
-                                    <Badge variant="outline" className="rounded-sm">
-                                        2 HSARING AC : Rs.3000/-
-                                    </Badge>
+                                    {[
+
+
+                                        {label: "Experience (Years)", value: listing?.agentExperience},
+                                        {label: "License Number", value: listing?.agentLicenseNumber},
+                                        {label: "Operating Since", value: listing?.agentOperatingSince},
+
+                                        {label: "Total Listings", value: listing?.agentTotalListings},
+
+                                        // { label: "Created At", value: listing?.v_created_date + ' ' + listing?.v_created_time },
+                                    ].map(
+                                        (item, index) => (item.value ? <Badge variant="outline" className="rounded-sm shadow">
+                                            {item.label} : {String(item.value)}
+                                        </Badge> : <></>))
+                                    }
+
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="wrapper">
+                            <div className="marquee">
+                                <div className="flex overflow-x-auto gap-2">
+                                    {
+                                        listing?.agentSpecializations && (
+                                            <Badge variant="outline" className="rounded-sm bg-rose-500 text-white">SPECIALIZATION</Badge>)}
+
+                                    {listing?.agentSpecializations?.map(a => (
+
+                                        <Badge variant="outline"
+                                               className="rounded-sm shadow"> {a}</Badge>
+                                    ))}
+
                                 </div>
                             </div>
                         </div>
 
                         <div className="flex justify-center mt-2">
-                            <Avatar className="w-24 h-24">
-                                <AvatarFallback className="text-xl">
+                            <Avatar className="w-24 h-24 shadow">
+                                <AvatarFallback className="text-3xl">
                                     {listing?.agentNameInitial}
                                 </AvatarFallback>
                             </Avatar>
@@ -48,7 +89,7 @@ function AgentListingCard({listing, active}) {
                         <div className="p-0 tracking-wide text-center mt-2">
                             <Link href={`/agent/${listing?.agentId}`}>
                                 <h2 className="font-semibold text-lg line-clamp-1">{listing?.agentAgencyName}</h2>
-                                <h2 className="font-medium text-muted-foreground text-md line-clamp-1">{listing?.user?.firstName +' '+listing?.user?.lastName}</h2>
+                                <h2 className="font-medium text-muted-foreground text-md line-clamp-1">{listing?.user?.firstName + ' ' + listing?.user?.lastName}</h2>
 
                                 <div className="wrapper">
                                     <div className="marquee animate-14">
@@ -73,23 +114,23 @@ function AgentListingCard({listing, active}) {
                         <div className="p-1">
 
                             <div className="wrapper">
-                                     <div className="marquee">
-                                        <div className="flex overflow-x-auto gap-2">
-                                            <Badge variant="outline" className="rounded-sm">
-                                                2400 sqft.
-                                            </Badge>
-                                            <Badge variant="outline" className="rounded-sm">
-                                                2 HSARING AC : Rs.3000/-
-                                            </Badge>
-                                        </div>
+                                <div className="marquee">
+                                    <div className="flex overflow-x-auto gap-2">
+                                        <Badge variant="outline" className="rounded-sm">
+                                            2400 sqft.
+                                        </Badge>
+                                        <Badge variant="outline" className="rounded-sm">
+                                            2 HSARING AC : Rs.3000/-
+                                        </Badge>
                                     </div>
-                             </div>
+                                </div>
+                            </div>
 
                             <div className="tracking-wide">
                                 <h2 className="font-semibold text-lg line-clamp-1">{listing?.agentAgencyName}</h2>
-                                <h2 className="font-medium text-muted-foreground text-md line-clamp-1">{listing?.user?.firstName +' '+listing?.user?.lastName}</h2>
+                                <h2 className="font-medium text-muted-foreground text-md line-clamp-1">{listing?.user?.firstName + ' ' + listing?.user?.lastName}</h2>
 
-    <p className="my-1  text-xs text-muted-foreground">{listing?.agentOfficeAddress}</p>
+                                <p className="my-1  text-xs text-muted-foreground">{listing?.agentOfficeAddress}</p>
 
                                 <p className="  text-sm mt-2 ">{listing?.agentBio}</p>
                             </div>
--- src/app/agent/[id]/page.jsx
+++ src/app/agent/[id]/page.jsx
@@ -7,6 +7,7 @@ import {Badge} from "@/components/ui/badge";
 import ShareButton from "@/app/property/[id]/_components/ShareButton";
 import {Avatar, AvatarFallback} from "@/components/ui/avatar";
 import "./pg-colive.css"
+import {Card, CardHeader} from "@/components/ui/card";
 
 async function AgentDetail({params}) {
     const {id} = params;
@@ -48,143 +49,159 @@ async function AgentDetail({params}) {
             </Head>
             <div className="px-2 md:px-6  my-3 mt-5 ">
 
-                <div className="flex overflow-x-auto gap-2">
-                    {listing?.agentPartnerType ? ( <Badge
-                        className="tracking-wider rounded-sm bg-yellow-600 text-white flex gap-1 animate-blink">
-                            <Award size={14} className="text-yellow-300"/><span>{listing?.agentPartnerType}</span></Badge>)
-                        : <></>}
-                    {listing?.agentIsVerified ?
-                        <Badge
-                            className="tracking-wider rounded-sm bg-green-600 text-white flex gap-1 animate-blink"><CheckLine
-                            size={14}/><span>Verified</span></Badge>
-                        : <></>}
-                    <Badge variant="outline" className="rounded-sm">
-                        Total Listing : {listing?.agentTotalListings}
-                    </Badge>
-                    {[
+                <Card className="rounded-sm bg-muted/50 border-none">
+                    <CardHeader>
+                        <div className="flex overflow-x-auto gap-2">
+                            {listing?.agentPartnerType ? ( <Badge
+                                    className="tracking-wider rounded-sm bg-yellow-600 text-white flex gap-1 animate-blink">
+                                    <Award size={14} className="text-yellow-300"/><span>{listing?.agentPartnerType}</span></Badge>)
+                                : <></>}
+                            {listing?.agentIsVerified ?
+                                <Badge
+                                    className="tracking-wider rounded-sm bg-green-600 text-white flex gap-1 animate-blink"><CheckLine
+                                    size={14}/><span>Verified</span></Badge>
+                                : <></>}
+                            <Badge variant="outline" className="rounded-sm">
+                                Total Listing : {listing?.agentTotalListings}
+                            </Badge>
+                            {[
 
 
-                        {label: "Experience (Years)", value: listing?.agentExperience},
-                        {label: "License Number", value: listing?.agentLicenseNumber},
-                        {label: "Operating Since", value: listing?.agentOperatingSince},
+                                {label: "Experience (Years)", value: listing?.agentExperience},
+                                {label: "License Number", value: listing?.agentLicenseNumber},
+                                {label: "Operating Since", value: listing?.agentOperatingSince},
 
-                        {label: "Total Listings", value: listing?.agentTotalListings},
+                                {label: "Total Listings", value: listing?.agentTotalListings},
 
-                        // { label: "Created At", value: listing?.v_created_date + ' ' + listing?.v_created_time },
-                    ].map(
-                        (item, index) => (item.value ? <Badge variant="outline" className="rounded-sm">
-                            {item.label} : {String(item.value)}
-                        </Badge> : <></>))
-                    }
-
-
-                </div>
-
-                <div>
+                                // { label: "Created At", value: listing?.v_created_date + ' ' + listing?.v_created_time },
+                            ].map(
+                                (item, index) => (item.value ? <Badge variant="outline" className="rounded-sm">
+                                    {item.label} : {String(item.value)}
+                                </Badge> : <></>))
+                            }
 
 
-                    <div className="flex flex-wrap gap-4 items-center   my-4">
-                        <div className="flex justify-center  ">
-                            <Avatar className="w-24 h-24">
-                                <AvatarFallback className="text-2xl">
-                                    {listing?.agentNameInitial}
-                                </AvatarFallback>
-                            </Avatar>
                         </div>
-                        <div className=" tracking-wide   ">
-                            <h2 className="font-semibold text-lg line-clamp-1">{listing?.agentAgencyName}</h2>
-                            <h2 className="font-medium text-muted-foreground text-md line-clamp-1">{listing?.user?.firstName + ' ' + listing?.user?.lastName}</h2>
 
 
-                            <div className="flex overflow-x-auto gap-2 mt-1">
-                                <p className="text-xs text-muted-foreground tracking-normal">
-                                    {listing?.agentOfficeAddress?.toUpperCase()} - {listing?.agentCity?.toUpperCase()} - {listing?.agentState?.toUpperCase()} - {listing?.agentCountry?.toUpperCase()}
-                                </p>
+
+                        <div className="flex flex-wrap gap-4 items-center   my-4">
+                            <div className="flex justify-center  ">
+                                <Avatar className="w-24 h-24 shadow">
+                                    <AvatarFallback className="text-3xl">
+                                        {listing?.agentNameInitial}
+                                    </AvatarFallback>
+                                </Avatar>
+                            </div>
+                            <div className=" tracking-wide   ">
+                                <h2 className="font-semibold text-lg line-clamp-1">{listing?.agentAgencyName}</h2>
+                                <h2 className="font-medium text-muted-foreground text-md line-clamp-1">{listing?.user?.firstName + ' ' + listing?.user?.lastName}</h2>
+
+
+                                <div className="flex overflow-x-auto gap-2 mt-1">
+                                    <p className="text-xs text-muted-foreground tracking-normal">
+                                        {listing?.agentOfficeAddress?.toUpperCase()} - {listing?.agentCity?.toUpperCase()} - {listing?.agentState?.toUpperCase()} - {listing?.agentCountry?.toUpperCase()}
+                                    </p>
+                                </div>
+
+
+                            </div>
+                        </div>
+
+
+
+
+                            <div className="flex-wrap flex items-center gap-2 my-4">
+                                <Link className=" "
+                                      href={`https://wa.me/9631045873?text=Hi! I want detail regarding property id ${listing?.agentId}`}
+                                      target="_blank">
+                                    <Button className="bg-green-500 text-white hover:bg-green-600 flex gap-1 items-center">
+                                        <MessageCircle/>
+                                        Whatsapp
+                                    </Button>
+                                </Link>
+
+                                <Button className="bg-pink-500 text-white hover:bg-pink-600 flex gap-1 items-center ">
+                                    <Phone/> Contact
+                                </Button>
+
+
+                                <Button className="bg-purple-500 text-white hover:bg-purple-600 flex gap-1 items-center ">
+                                    <Mail/> Email
+                                </Button>
+
+                                <Link   href={listing?.agentWebsite}
+                                        target="_blank">
+                                    <Button className="bg-sky-500 text-white hover:bg-sky-600 flex gap-1 items-center">
+                                        <Globe/>
+                                        Website
+                                    </Button>
+                                </Link>
+
+                                <ShareButton/>
+
                             </div>
 
 
+
+                        <div className="mt-2 flex flex-wrap gap-2 items-center">
+                            <p className="text-xs text-muted-foreground">Agent Id : {listing?.agentId}</p>
+                            <div className=" sm:ml-auto">
+                                {listing?.agentIsVerified ?
+                                    <Badge
+                                        className="tracking-wider rounded-sm bg-green-600 text-white flex gap-1 animate-blink"><CheckLine
+                                        size={14}/><span>Verified</span></Badge>
+                                    : <></>}
+                            </div>
                         </div>
-                    </div>
-                </div>
+
+                    </CardHeader>
+                </Card>
+
+                <Card className="rounded-sm bg-muted/50 border-none my-6">
+                    <CardHeader>  <h2 className="font-bold text-xl tracking-wide">Overview</h2>
+                        <p className="my-2 whitespace-pre w-full overflow-hidden  ">{listing?.agentBio}</p>
+                    </CardHeader>
+
+                </Card>
+
+                <Card className="rounded-sm bg-muted/50 border-none my-6">
+                    <CardHeader>
+                        <h2 className="font-bold text-xl tracking-wide">Specialization</h2>
+                        <div className="my-2  w-full flex flex-wrap gap-2 ">{listing?.agentSpecializations?.map(a => (<Badge variant="secondary" className="p-1 rounded-sm">{a}</Badge>))}</div>
+
+                    </CardHeader>
+
+                </Card>
 
 
-                <div className="mb-6 flex gap-2 flex-col">
+                <Card className="rounded-sm bg-muted/50 border-none my-6">
+                    <CardHeader>
+                        <h2 className="font-bold text-xl tracking-wide">Areas Served</h2>
+                        <div className="my-2  w-full flex flex-wrap gap-2 ">{listing?.agentAreasServed?.map(a => (<Badge variant="secondary" className="p-1 rounded-sm">{a}</Badge>))}</div>
 
+                    </CardHeader>
 
-                    <div className="flex-wrap flex items-center gap-2 my-2">
-                        <Link className=" "
-                              href={`https://wa.me/9631045873?text=Hi! I want detail regarding property id ${listing?.agentId}`}
-                              target="_blank">
-                            <Button className="bg-green-500 text-white hover:bg-green-600 flex gap-1 items-center">
-                                <MessageCircle/>
-                                Whatsapp
-                            </Button>
-                        </Link>
+                </Card>
 
-                        <Button className="bg-pink-500 text-white hover:bg-pink-600 flex gap-1 items-center ">
-                            <Phone/> Contact
-                        </Button>
+                <Card className="rounded-sm bg-muted/50 border-none my-6">
+                    <CardHeader>
+                        <h2 className="font-bold text-xl tracking-wide">Language</h2>
+                        <div className="my-2  w-full flex flex-wrap gap-2 ">{listing?.agentLanguagesSpoken?.map(a => (<Badge variant="secondary" className="p-1 rounded-sm">{a}</Badge>))}</div>
+                    </CardHeader>
 
+                </Card>
 
-                        <Button className="bg-purple-500 text-white hover:bg-purple-600 flex gap-1 items-center ">
-                            <Mail/> Email
-                        </Button>
+                <Card className="rounded-sm bg-muted/50 border-none my-6">
+                    <CardHeader>
+                        <h2 className="font-bold text-xl tracking-wide ">Location</h2>
+                        <p className="mt-2 whitespace-pre w-full overflow-hidden  ">{listing?.agentOfficeAddress}</p>
+                        <p className=" whitespace-pre w-full overflow-hidden  ">{listing?.agentCity}</p>
+                        <p className=" whitespace-pre w-full overflow-hidden  ">{listing?.agentState}</p>
+                        <p className=" whitespace-pre w-full overflow-hidden  ">{listing?.agentCountry}</p>
+                    </CardHeader>
 
-                        <Link   href={listing?.agentWebsite}
-                              target="_blank">
-                            <Button className="bg-sky-500 text-white hover:bg-sky-600 flex gap-1 items-center">
-                                <Globe/>
-                                Website
-                            </Button>
-                        </Link>
-
-                        <ShareButton/>
-
-                    </div>
-
-
-                </div>
-
-                <div className="my-6 flex flex-wrap gap-2 items-center">
-                    <p className="text-xs text-muted-foreground">Agent Id : {listing?.agentId}</p>
-                    <div className=" sm:ml-auto">
-                        {listing?.agentIsVerified ?
-                            <Badge
-                                className="tracking-wider rounded-sm bg-green-600 text-white flex gap-1 animate-blink"><CheckLine
-                                size={14}/><span>Verified</span></Badge>
-                            : <></>}
-                    </div>
-                </div>
-                <div className="my-6">
-                    <h2 className="font-bold text-xl tracking-wide">Overview</h2>
-                    <p className="my-2 whitespace-pre w-full overflow-hidden  ">{listing?.agentBio}</p>
-                </div>
-
-                <div className="my-6">
-                    <h2 className="font-bold text-xl tracking-wide">Specialization</h2>
-                    <div className="my-2  w-full flex flex-wrap gap-2 ">{listing?.agentSpecializations?.map(a => (<Badge variant="secondary" className="p-1 rounded-sm">{a}</Badge>))}</div>
-                </div>
-
-
-                <div className="my-6">
-                    <h2 className="font-bold text-xl tracking-wide">Areas Served</h2>
-                    <div className="my-2  w-full flex flex-wrap gap-2 ">{listing?.agentAreasServed?.map(a => (<Badge variant="secondary" className="p-1 rounded-sm">{a}</Badge>))}</div>
-                </div>
-
-                <div className="my-6">
-                    <h2 className="font-bold text-xl tracking-wide">Language</h2>
-                    <div className="my-2  w-full flex flex-wrap gap-2 ">{listing?.agentLanguagesSpoken?.map(a => (<Badge variant="secondary" className="p-1 rounded-sm">{a}</Badge>))}</div>
-                </div>
-
-                <div className="my-6">
-                    <h2 className="font-bold text-xl tracking-wide ">Location</h2>
-                    <p className="mt-2 whitespace-pre w-full overflow-hidden  ">{listing?.agentOfficeAddress}</p>
-                    <p className=" whitespace-pre w-full overflow-hidden  ">{listing?.agentCity}</p>
-                    <p className=" whitespace-pre w-full overflow-hidden  ">{listing?.agentState}</p>
-                    <p className=" whitespace-pre w-full overflow-hidden  ">{listing?.agentCountry}</p>
-
-
-                </div>
+                </Card>
 
 
             </div>
